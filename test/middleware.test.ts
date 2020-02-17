69e809f6a3e84fc24917422df59762dfbf667f30
Gregor Martynus <gregor@martynus.net>
2020-02-17 15:00:46 -0800

    test(middleware): `GET /api/github/oauth/login?state=mystate123&scopes=one,two,three`

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -1,4 +1,5 @@
 import { createServer } from "http";
+import { URL } from "url";
 
 import fetch from "node-fetch";
 
@@ -44,8 +45,46 @@ describe("app.middleware", () => {
     server.close();
 
     expect(status).toEqual(302);
-    expect(headers.get("location")).toMatch(
-      /^https:\/\/github.com\/login\/oauth\/authorize\?allow_signup=true&client_id=0123&state=\w+$/
+
+    const url = new URL(headers.get("location") as string);
+    expect(url).toMatchObject({
+      origin: "https://github.com",
+      pathname: "/login/oauth/authorize"
+    });
+    expect(url.searchParams.get("client_id")).toEqual("0123");
+    expect(url.searchParams.get("state")).toMatch(/^\w+$/);
+    expect(url.searchParams.get("scope")).toEqual(null);
+  });
+
+  it("GET /api/github/oauth/login?state=mystate123&scopes=one,two,three", async () => {
+    const app = new OAuthApp({
+      clientId: "0123",
+      clientSecret: "0123secret"
+    });
+
+    const server = createServer(app.middleware).listen();
+    // @ts-ignore complains about { port } although it's included in returned AddressInfo interface
+    const { port } = server.address();
+
+    const { status, headers } = await fetch(
+      `http://localhost:${port}/api/github/oauth/login?state=mystate123&scopes=one,two,three`,
+      {
+        redirect: "manual"
+      }
     );
+
+    server.close();
+
+    expect(status).toEqual(302);
+
+    const url = new URL(headers.get("location") as string);
+    expect(url).toMatchObject({
+      origin: "https://github.com",
+      pathname: "/login/oauth/authorize"
+    });
+
+    expect(url.searchParams.get("client_id")).toEqual("0123");
+    expect(url.searchParams.get("state")).toEqual("mystate123");
+    expect(url.searchParams.get("scope")).toEqual("one,two,three");
   });
 });
