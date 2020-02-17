18a69cb25d5ee5789e2ef9996eddaa1b98ca97a1
Gregor Martynus <gregor@martynus.net>
2020-02-17 14:39:16 -0800

    test(middleware): `GET /api/github/oauth/login`

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -1,6 +1,6 @@
 import { createServer } from "http";
 
-import { request } from "@octokit/request";
+import fetch from "node-fetch";
 
 import { OAuthApp } from "../src";
 
@@ -15,12 +15,37 @@ describe("app.middleware", () => {
     // @ts-ignore complains about { port } although it's included in returned AddressInfo interface
     const { port } = server.address();
 
-    const { data } = await request(
+    const response = await fetch(
       `http://localhost:${port}/api/github/oauth/octokit.js`
     );
 
-    expect(data).toMatch(/Core.defaults/);
+    server.close();
+
+    expect(await response.text()).toMatch(/Core.defaults/);
+  });
+
+  it("GET /api/github/oauth/login", async () => {
+    const app = new OAuthApp({
+      clientId: "0123",
+      clientSecret: "0123secret"
+    });
+
+    const server = createServer(app.middleware).listen();
+    // @ts-ignore complains about { port } although it's included in returned AddressInfo interface
+    const { port } = server.address();
+
+    const { status, headers } = await fetch(
+      `http://localhost:${port}/api/github/oauth/login`,
+      {
+        redirect: "manual"
+      }
+    );
 
     server.close();
+
+    expect(status).toEqual(302);
+    expect(headers.get("location")).toMatch(
+      /^https:\/\/github.com\/login\/oauth\/authorize\?allow_signup=true&client_id=0123&state=\w+$/
+    );
   });
 });
