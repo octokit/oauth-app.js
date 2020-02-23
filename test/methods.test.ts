3cfd2154679e3988965aaca99477cb0b5c48e34b
Gregor Martynus <gregor@martynus.net>
2020-02-23 15:20:41 -0800

    test: `createToken(options)`

--- test/methods.test.ts
+++ test/methods.test.ts
@@ -1,4 +1,6 @@
-import { getAuthorizationUrl } from "../src";
+import nock from "nock";
+
+import { getAuthorizationUrl, createToken } from "../src";
 
 describe("app", () => {
   it("getAuthorizationUrl", () => {
@@ -13,5 +15,29 @@ describe("app", () => {
     expect(url).toStrictEqual(
       "https://github.com/login/oauth/authorize?allow_signup=true&client_id=0123&state=state123"
     );
+  });
+
+  it("createToken", () => {
+    expect(createToken).toBeInstanceOf(Function);
+  });
+
+  it("createToken(options)", async () => {
+    nock("https://github.com")
+      .post("/login/oauth/access_token")
+      .reply(200, {
+        access_token: "token123",
+        scope: "repo,gist",
+        token_type: "bearer"
+      });
+
+    const { token, scopes } = await createToken({
+      clientId: "0123",
+      clientSecret: "0123secret",
+      state: "state123",
+      code: "code123"
+    });
+
+    expect(token).toEqual("token123");
+    expect(scopes).toEqual(["repo", "gist"]);
   });
 });
