ac5846ba2d875c2ba403d008d7153bf29d489df7
Gregor Martynus <gregor@martynus.net>
2020-02-23 15:20:31 -0800

    test: `app.createToken(options)`

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,11 +1,17 @@
+import nock from "nock";
+
 import { OAuthApp } from "../src";
 
 describe("app", () => {
-  it("app.getAuthorizationUrl is function", () => {
-    const app = new OAuthApp({
+  let app: OAuthApp;
+
+  beforeEach(() => {
+    app = new OAuthApp({
       clientId: "0123",
       clientSecret: "0123secret"
     });
+  });
+  it("app.getAuthorizationUrl", () => {
     expect(app.getAuthorizationUrl).toBeInstanceOf(Function);
   });
 
@@ -20,5 +26,27 @@ describe("app", () => {
     expect(url).toStrictEqual(
       "https://github.com/login/oauth/authorize?allow_signup=true&client_id=0123&state=state123"
     );
+  });
+
+  it("app.createToken", () => {
+    expect(app.createToken).toBeInstanceOf(Function);
+  });
+
+  it("createToken(options)", async () => {
+    nock("https://github.com")
+      .post("/login/oauth/access_token")
+      .reply(200, {
+        access_token: "token123",
+        scope: "repo,gist",
+        token_type: "bearer"
+      });
+
+    const { token, scopes } = await app.createToken({
+      state: "state123",
+      code: "code123"
+    });
+
+    expect(token).toEqual("token123");
+    expect(scopes).toEqual(["repo", "gist"]);
   });
 });
