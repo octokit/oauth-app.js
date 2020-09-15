494365f97955863bda01bf8fbc4582fed6785daa
Gregor Martynus <gregor@martynus.net>
2020-09-15 15:52:19 -0700

    test: `?scope` query parameter on GitHub OAuth authorization URL set from `.defaultScopes` option

--- test/node-middleware.test.ts
+++ test/node-middleware.test.ts
@@ -36,6 +36,38 @@ describe("getNodeMiddleware(app)", () => {
     expect(url.searchParams.get("scope")).toEqual(null);
   });
 
+  it("GET /api/github/oauth/login with defaultScopes (#110)", async () => {
+    const app = new OAuthApp({
+      clientId: "0123",
+      clientSecret: "0123secret",
+      defaultScopes: ["repo"],
+    });
+
+    const server = createServer(getNodeMiddleware(app)).listen();
+    // @ts-ignore complains about { port } although it's included in returned AddressInfo interface
+    const { port } = server.address();
+
+    const { status, headers } = await fetch(
+      `http://localhost:${port}/api/github/oauth/login`,
+      {
+        redirect: "manual",
+      }
+    );
+
+    server.close();
+
+    expect(status).toEqual(302);
+
+    const url = new URL(headers.get("location") as string);
+    expect(url).toMatchObject({
+      origin: "https://github.com",
+      pathname: "/login/oauth/authorize",
+    });
+    expect(url.searchParams.get("client_id")).toEqual("0123");
+    expect(url.searchParams.get("state")).toMatch(/^\w+$/);
+    expect(url.searchParams.get("scope")).toEqual("repo");
+  });
+
   it("GET /api/github/oauth/login?state=mystate123&scopes=one,two,three", async () => {
     const app = new OAuthApp({
       clientId: "0123",
