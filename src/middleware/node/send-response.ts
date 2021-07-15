d3a7b388a21be194140b10246a526231a8857765
Baoshan Sheng <sheng@icmd.org>
2021-07-16 02:19:35 +0800

    refactor: runtime-agnostic HTTP handler (#247)

---
+++ src/middleware/node/send-response.ts
@@ -0,0 +1,13 @@
+// remove type imports from http for Deno compatibility
+// see https://github.com/octokit/octokit.js/issues/2075#issuecomment-817361886
+// import { IncomingMessage, ServerResponse } from "http";
+type ServerResponse = any;
+import { OctokitResponse } from "../types";
+
+export function sendResponse(
+  octokitResponse: OctokitResponse,
+  response: ServerResponse
+) {
+  response.writeHead(octokitResponse.status, octokitResponse.headers);
+  response.end(octokitResponse.text);
+}
