0680ff096789cc0c86d1cda6e8ca874123875dc3
Baoshan Sheng <sheng@icmd.org>
2021-07-17 01:12:17 +0800

    feat: cloudflare event handler (#249)

---
+++ src/middleware/cloudflare/parse-request.ts
@@ -0,0 +1,12 @@
+import { OctokitRequest } from "../types";
+
+export function parseRequest(request: Request): OctokitRequest {
+  // @ts-ignore Worker environment supports fromEntries/entries.
+  const headers = Object.fromEntries(request.headers.entries());
+  return {
+    method: request.method,
+    url: request.url,
+    headers,
+    text: () => request.text(),
+  };
+}
