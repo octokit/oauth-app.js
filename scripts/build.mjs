e59a54707e31075e4ff1d2501998978c3b702ff1
wolfy1339 <webmaster@wolfy1339.com>
2024-04-16 15:00:50 -0400

    fix(pkg): add a default fallback export

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -67,12 +67,13 @@ async function main() {
       {
         ...pkg,
         files: ["dist-*/**", "bin/**"],
-        main: "./dist-node/index.js",
         types: "./dist-types/index.d.ts",
         exports: {
           ".": {
             types: "./dist-types/index.d.ts",
             import: "./dist-node/index.js",
+            // Tooling currently are having issues with the "exports" field when there is no "default", ex: TypeScript, eslint
+            default: "./dist-node/index.js",
           },
         },
         sideEffects: false,
